ef7fb25c4e32867a198731d7d53b3e139f5f1017
Jorge Villarreal <jorgemvv01@gmail.com>
2023-03-07 13:06:58 -0500

    Add railway database variables

--- storage/storage.go
+++ storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var db *gorm.DB
@@ -10,7 +11,7 @@ var db *gorm.DB
 func GetInstance() *gorm.DB {
 	if db == nil {
 		var err error
-		dsn := "host=localhost user=admin password=000108 dbname=videoclub port=5432 sslmode=disable"
+		dsn := `host=` + os.Getenv("PGHOST") + ` user=` + os.Getenv("PGUSER") + ` password=` + os.Getenv("PGPASSWORD") + `dbname=` + os.Getenv("PGDATABASE") + ` port=` + os.Getenv("PGPORT") + ` sslmode=disable`
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 		if err != nil {
 			panic("failed to connect database")
